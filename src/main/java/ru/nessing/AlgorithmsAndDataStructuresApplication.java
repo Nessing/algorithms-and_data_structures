602b92abf749e6c16eb208055ce476d267401c88
Nessing <35237174+Nessing@users.noreply.github.com>
2021-12-04 17:45:53 +0300

    Create repository

---
+++ src/main/java/ru/nessing/AlgorithmsAndDataStructuresApplication.java
@@ -0,0 +1,13 @@
+package ru.nessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsAndDataStructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsAndDataStructuresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nessing/AlgorithmsAndDataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsAndDataStructuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
